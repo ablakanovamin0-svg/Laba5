dbcbce70d1d92313d06fbbf6184d4fadd7012fc7
ablakanovamin0-svg <ablakanovamin0@gmail.com>
2025-12-25 19:15:58 +0300

    Изменение класса Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        Injector<SomeBean> injector = new Injector<>();
+        SomeBean someBean = injector.inject(new SomeBean());
+        Class<?> objectClass = someBean.getClass();
+        System.out.println(objectClass);
+        someBean.foo();
+        SomeBean otherBean = (new Injector<SomeBean>()).inject(new SomeBean());
+        Class<?> othetObjectClass = someBean.getClass();
+        System.out.println(othetObjectClass);
+        otherBean.foo();
     }
 }
