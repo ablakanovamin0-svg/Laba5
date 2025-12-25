dd16afd12e30936132c13e35b5b5f42325bc1b73
ablakanovamin0-svg <ablakanovamin0@gmail.com>
2025-12-25 19:11:23 +0300

    Создание класса Injector

---
+++ src/main/java/org/example/Injector.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import javax.management.InstanceNotFoundException;
+import java.util.Properties;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Field;
+
+public class Injector<T> {
+    public T inject(T object) {
+        Properties properties = new Properties();
+        try (InputStream inputStream = new FileInputStream("src/main/resources/config.properties")) {
+            properties.load(inputStream);
+        }
+        catch (IOException ioException) {
+            throw new RuntimeException(ioException);
+        }
+        Class<?> classOfObject = object.getClass();
+        Field[] fields = classOfObject.getDeclaredFields();
+        for (Field field : fields) {
+            Annotation[] annotations = field.getAnnotations();
+            for (Annotation annotation : annotations) {
+                if (annotation.annotationType().equals(AutoInjectable.class)) {
+                    String implementation = properties.getProperty(field.getType().getName());
+                    try {
+                        Class<?> implementationClass = Class.forName(implementation);
+                        Object instance = implementationClass.getDeclaredConstructor().newInstance();
+                        field.setAccessible(true);
+                        field.set(object, instance);
+                    }
+                    catch (ClassNotFoundException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException exception) {
+                        throw new RuntimeException(exception);
+                    }
+                }
+            }
+        }
+        return object;
+    }
+}
